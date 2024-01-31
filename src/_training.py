ed7e702e072df6d601d9bd6847e86a503f4edcc8
Richard C.J <91405303+richardcj2710@users.noreply.github.com>
2024-01-31 12:40:55 +0700

    Add files via upload

---
+++ src/_training.py
@@ -0,0 +1,13 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+import tensorflow as tf
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
